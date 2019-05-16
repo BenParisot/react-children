d9fc01e7a4987601297e58096af4dc11308d7cf1
Ben Parisot <me@benparisot.com>
2019-05-16 10:54:04 -0700

    create sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+export default Sidebar({ children }) {
+    return (
+        <nav>
+            {children}
+        </nav>
+    );
+}
+
+Sidebar.propTypes = {
+    children: PropTypes.node
+}
