ea707d1e32c0a4993f8135dae72a2ebd858d3703
Ben Parisot <me@benparisot.com>
2019-05-16 10:51:20 -0700

    write app component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+export default function App() {
+  return <Sidebar>
+    <li>Home</li>
+    <li>Projects</li>
+    <li>Blog</li>
+    <li>Contact</li>
+  </Sidebar>;
+}
