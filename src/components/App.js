7c5cda64a37eed1e57d2bd7affee9379f0e88216
Ben Parisot <me@benparisot.com>
2019-05-17 11:23:16 -0700

    write error catching component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,17 @@
-import React, { useState } from 'react';
-import Sidebar from './Sidebar';
-import Styles from './main.css';
+import React from 'react';
+import ErrorBoundary from './ErrorBoundary';
+import Bad from './Bad';
+import Good from './Good';
 
 export default function App() {
 
-  const [selected, updateSelected] = useState('home');
-
-  const response = {
-    home: 'Welcome home buddy',
-    projects: 'Check out these sweet projects',
-    blog: 'Oooooh read em',
-    contact: 'lets chat'
-  };
 
   return (
-    <section className={Styles.container}>
-      <Sidebar>
-        <li onClick={() => updateSelected('home')} >Home</li>
-        <li onClick={() => updateSelected('projects')} >Projects</li>
-        <li onClick={() => updateSelected('blog')} >Blog</li>
-        <li onClick={() => updateSelected('contact')} >Contact</li>
-      </Sidebar>
-      <p>{response[selected]}</p>
-    </section>
-
+    <>
+      <ErrorBoundary>
+        <Bad />
+      </ErrorBoundary>
+      <Good />
+    </>
   ); 
-  
 }
